2fbd4d1cbe72875ed3a73da9eda41805661efa7a
zack <zackyanto.leo@gmail.com>
2022-08-22 22:07:35 +0700

    tambah Jurusan

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 //Athif Zakiyanto
 console.log("Athif Zakiyanto")
-console.log("Universitas Gundrama") //Update dari branch develop
+console.log("Universitas Gundrama") //Update dari branch featureA
+console.log("Teknik Informatika") //Update dari branch featureB
